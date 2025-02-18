ec90f52d898e57edd66f7a1d812fe7efb5adce7a
Emre İnan <emreinannn@gmail.com>
2025-02-18 15:51:26 +0300

    Add project files.

---
+++ src/eCommerce/WebAPI/Program.cs
@@ -0,0 +1,19 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+app.MapOpenApi();
+app.MapScalarApiReference();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
